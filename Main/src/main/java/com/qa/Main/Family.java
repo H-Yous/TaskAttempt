43483e5838ad1a9f73ec06d9cd45130fb7ce1b1f
Humza Younus <humzayounus@Humzas-MacBook-Pro.local>
2019-05-13 17:09:03 +0100

    Final commit, project Finished, further tests required

--- Main/src/main/java/com/qa/Main/Family.java
+++ Main/src/main/java/com/qa/Main/Family.java
@@ -5,88 +5,252 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Family {
-	List <People> person = new ArrayList<People>();
-	
+	List<People> person = new ArrayList<People>();
+
 	public boolean male(String name) {
-		if(ishere(name)) {
+		if (ishere(name)) {
 			People checkPerson = getPerson(name);
-			
-			if(checkPerson.getGender().equals("None")) {
+			if (checkMaleSpouse(name)) {
+				return false;
+			} else if (checkPerson.getGender().equals("None")) {
 				checkPerson.setGender("Male");
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
-		} 
-		else {
+		} else {
 			person.add(new People(name));
 			People newPerson = getPerson(name);
 			newPerson.setGender("Male");
 			return true;
 		}
 	}
-	
+
 	public boolean female(String name) {
-		if(ishere(name)) {
+		if (ishere(name)) {
 			People checkPerson = getPerson(name);
-			
-			if(checkPerson.getGender().equals("None")) {
+
+			if (checkFemaleSpouse(name)) {
+				return false;
+			} else if (checkPerson.getGender().equals("None")) {
 				checkPerson.setGender("Female");
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
-		} 
-		else {
+		} else {
 			person.add(new People(name));
 			People newPerson = getPerson(name);
 			newPerson.setGender("Female");
 			return true;
 		}
 	}
-	
-	public boolean isMale(String name) { 
 
-		if(ishere(name)) { 
-			People person2 = getPerson(name); 
+	public boolean isMale(String name) {
 
-			if(person2.getGender().equals("Male")) { 
+		if (ishere(name)) {
+			People person2 = getPerson(name);
+
+			if (person2.getGender().equals("Male")) {
 				return true;
+			} else {
+				return false;
 			}
-			else { return false; } 
-		}
-		else { 
-			person.add(new People(name)); 
+		} else {
+			person.add(new People(name));
 			return false;
 		}
 
 	}
 
 	public boolean isFemale(String name) {
-		if(ishere(name)) { 
-			People person2 = getPerson(name); 
+		if (ishere(name)) {
+			People person2 = getPerson(name);
 
-			if(person2.getGender().equals("Female")) { 
+			if (person2.getGender().equals("Female")) {
 				return true;
+			} else {
+				return false;
 			}
-			else { return false; } 
-		}
-		else { 
-			person.add(new People(name)); 
+		} else {
+			person.add(new People(name));
 			return false;
 		}
 	}
 
+	public Boolean setParentOf(String childName, String parentName) {
+		if (ishere(parentName)) {
+			People parent = getPerson(parentName);
+			
+
+			if (ishere(childName)) {
+				People child = getPerson(childName);
+
+				if (isChild(parentName, childName)) {
+					return false;
+				} else {
+					if (child.getParentList().get().size() == 0) {
+						child.getParentList().get().add(parent);
+						parent.getChildList().get().add(child);
+						return true;
+					} else if (child.getParentList().get().size() == 1) {
+						if (child.getParentList().get().get(0).getGender().equals("None")) {
+
+							child.getParentList().get().add(parent);
+							parent.getChildList().get().add(child);
+
+							child.getParentList().get().get(0).getSpouseList().get().add(parent);
+							parent.getSpouseList().get().add(child.getParentList().get().get(0));
+							return true;
+						} else if (!child.getParentList().get().get(0).getGender().equals(parent.getGender())) {
+							child.getParentList().get().add(parent);
+							parent.getChildList().get().add(child);
+
+							child.getParentList().get().get(0).getSpouseList().get().add(parent);
+							parent.getSpouseList().get().add(child.getParentList().get().get(0));
+							return true;
+						}
+						else {
+							return false;
+						}
+					} else {return false;}
+				}
+			} else {person.add(new People(childName));
+			People child = getPerson(childName);
+			parent.getChildList().get().add(child);
+			child.getParentList().get().add(parent);
+
+			return true;
+			}
+		}
+		else {
+			person.add(new People(parentName));
+			People parent = getPerson(parentName);
+			
+			if (ishere(childName)) {
+				People child = getPerson(childName);
+
+				if (isChild(parentName, childName)) {
+					return false;
+				} else {
+					if (child.getParentList().get().size() == 0) {
+						child.getParentList().get().add(parent);
+						parent.getChildList().get().add(child);
+						return true;
+					} else if (child.getParentList().get().size() == 1) {
+						if (child.getParentList().get().get(0).getGender().equals("None")) {
+
+							child.getParentList().get().add(parent);
+							parent.getChildList().get().add(child);
+
+							child.getParentList().get().get(0).getSpouseList().get().add(parent);
+							parent.getSpouseList().get().add(child.getParentList().get().get(0));
+							return true;
+						} else if (!child.getParentList().get().get(0).getGender().equals(parent.getGender())) {
+							child.getParentList().get().add(parent);
+							parent.getChildList().get().add(child);
+
+							child.getParentList().get().get(0).getSpouseList().get().add(parent);
+							parent.getSpouseList().get().add(child.getParentList().get().get(0));
+							return true;
+						}
+						else {
+							return false;
+						}
+					} else {return false;}
+				}
+			} else {person.add(new People(childName));
+			People child = getPerson(childName);
+			parent.getChildList().get().add(child);
+			child.getParentList().get().add(parent);
+
+			return true;
+			}
+		}
+	}
+	
+	
+	public String[] getParentsOf(String name) {
+		String[] parents = new String[2];
+		if(ishere(name)) {
+			People child = getPerson(name);
+			List <People> parentList = child.getParentList().get();
+			
+			List<String> names = parentList.stream()
+					.map(x -> x.getName())
+					.sorted()
+					.collect(Collectors.toList());
+			
+			parents = names.stream().toArray(String[]::new);
+			
+			return parents;
+		} else {
+			person.add(new People(name));
+			return parents;
+		}
+	}
+
+	
+	public String[] getChildrenOf(String name) {
+		if(ishere(name)) {
+			People parent = getPerson(name);
+			String[] children = new String[parent.getChildList().get().size()];
+
+			List <String> names = parent.getChildList().get().stream().map(x -> x.getName()).sorted().collect(Collectors.toList());
+			children = names.stream().toArray(String[]::new);
+
+
+			return children;
+		}
+		else {
+			People parent = getPerson(name);
+			String[] children = new String[parent.getChildList().get().size()];
+			
+			return children;
+		}
+	}
+	
+	
+	
+	
+	
+
+	public Boolean isChild(String childName, String parentName) {
+		People parent = getPerson(parentName);
+
+		boolean ishe = parent.getChildList().get().stream().anyMatch(x -> x.getName().equals(childName));
+		return ishe;
+	}
 
 	public boolean ishere(String name) {
 		boolean ishe = person.stream().anyMatch(x -> x.getName().equals(name));
 		return ishe;
 	}
-	
+
 	public People getPerson(String name) {
 		List<People> person2 = person.stream().filter(x -> x.getName().equals(name)).collect(Collectors.toList());
 		return person2.get(0);
+	}
+
+	public Boolean checkMaleSpouse(String name) {
+		People checkP = getPerson(name);
+		if (checkP.getSpouseList().get().size() > 0) {
+			if (isMale(checkP.getSpouseList().get().get(0).getName())) {
+				return true;
+			} else
+				return false;
+		} else
+			return false;
+	}
+
+	public Boolean checkFemaleSpouse(String name) {
+		People checkP = getPerson(name);
+		if (checkP.getSpouseList().get().size() > 0) {
+			if (isFemale(checkP.getSpouseList().get().get(0).getName())) {
+				return true;
+			} else
+				return false;
+		} else
+			return false;
 	}
 }
--- Main/src/test/java/com/qa/Main/TestClass.java
+++ Main/src/test/java/com/qa/Main/TestClass.java
@@ -1,74 +1,131 @@
 package com.qa.Main;
 
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-import junit.framework.TestCase;
-
-public class TestClass extends TestCase {
+public class TestClass {
 	
 	Family fam = new Family();
+	
 	@Test
 	public void testMale() {
 		assertTrue(fam.male("Dylan"));
 	}
 	
+	@Test
 	public void testMaleIfAssigned() {
 		fam.male("Dylan");
 		assertFalse(fam.male("Dylan"));
 	}
 	
+	@Test
 	public void testMaleOfMadePerson() {
 		fam.person.add(new People("Dylan"));
 		fam.person.get(0).setGender("Male");
 		assertFalse(fam.male("Dylan"));
 	}
 	
+	@Test
 	public void testFemale() {
 		assertTrue(fam.female("Naomi"));
 	}
 	
+	@Test
 	public void testFemaleIfAssigned() {
 		fam.female("Naomi");
 		assertFalse(fam.female("Naomi"));
 	}
 	
+	@Test
 	public void testFemaleOfMadePerson() {
 		fam.person.add(new People("Naomi"));
 		fam.person.get(0).setGender("Female");
 		assertFalse(fam.female("Naomi"));
 	}
 	
+	@Test
 	public void testIfisMale() {
 		fam.person.add(new People("Humza"));
 		fam.person.get(0).setGender("Male");
 		assertTrue(fam.isMale("Humza"));
 	}
 	
+	@Test
 	public void testIfisMaleAssigned() {
 		fam.person.add(new People("Humza"));
 		fam.person.get(0).setGender("Female");
 		assertFalse(fam.isMale("Humza"));
 	}
 	
+	@Test
 	public void testIfisMaleNotAssigned() {
 		assertFalse(fam.isMale("Humza"));
 	}
 	
+	@Test
 	public void testIfisFemale() {
 		fam.person.add(new People("Naomi"));
 		fam.person.get(0).setGender("Female");
 		assertTrue(fam.isFemale("Naomi"));
 	}
 	
+	@Test
 	public void testIfisFemaleAssigned() {
 		fam.person.add(new People("Naomi"));
 		fam.person.get(0).setGender("Male");
 		assertFalse(fam.isFemale("Naomi"));
 	}
 	
+	@Test
 	public void testIfisFemaleNotAssigned() {
 		assertFalse(fam.isFemale("Naomi"));
 	}
+	
+	@Test
+	public void testSetParentsNoneExist() {
+		assertTrue(fam.setParentOf("Humza", "Farhaan"));
+	}
+	
+	@Test
+	public void testSetParentChildExists() {
+		fam.person.add(new People("Humza"));
+		assertTrue(fam.setParentOf("Humza", "Farhaan"));
+	}
+	
+	@Test
+	public void testSetParentParentExists() {
+		fam.person.add(new People("Farhaan"));
+		assertTrue(fam.setParentOf("Humza", "Farhaan"));
+	}
+	
+	@Test
+	public void testSetParentBothExist() {
+		fam.person.add(new People("Farhaan"));
+		fam.person.add(new People("Humza"));
+		assertTrue(fam.setParentOf("Humza", "Farhaan"));
+	}
+	
+	@Test
+	public void getParentsof() {
+		fam.person.add(new People("Ali"));
+		fam.person.add(new People("Dan"));
+		fam.setParentOf("Ali", "Dan");
+		fam.setParentOf("Ali", "Linda");
+		fam.getParentsOf("Ali");
+		assertEquals(fam.getParentsOf("Ali"), "Dan Linda");
+		
+	}
+	
+	@Test
+	public void getChildrenOf() {
+		
+	}
+	
 	
 //	public void testMaleWhenSpouseMale() {
 //		
